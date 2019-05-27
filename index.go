6769fff69cb5a041a813cc72187e3477991f912c
Anthony Voutas <voutasaurus@gmail.com>
2019-05-27 18:15:03 +1000

    add telegram chatbot with basic kv using env vars

---
+++ index.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+var (
+	callbackURL = "https://api.telegram.org/bot" + os.Getenv("BOT_TOKEN") + "/sendMessage"
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	var v telegram
+	if err := json.NewDecoder(r.Body).Decode(&v); err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+	if err := v.Validate(); err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+	if err := handle(&v); err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+}
+
+type telegram struct {
+	UpdateID int `json:"update_id"`
+	Message  struct {
+		MessageID int `json:"message_id"`
+		From      struct {
+			ID           int    `json:"id"`
+			IsBot        bool   `json:"is_bot"`
+			FirstName    string `json:"first_name"`
+			LastName     string `json:"last_name"`
+			LanguageCode string `json:"language_code"`
+		} `json:"from"`
+		Chat struct {
+			ID        int    `json:"id"`
+			FirstName string `json:"first_name"`
+			LastName  string `json:"last_name"`
+			Type      string `json:"type"`
+		} `json:"chat"`
+		Date int    `json:"date"`
+		Text string `json:"text"`
+	} `json:"message"`
+}
+
+func (t telegram) Validate() error {
+	fmt.Fprintf(os.Stderr, "%+v", t)
+	return nil
+}
+
+func handle(t *telegram) error {
+	value, err := lookup(t.Message.Text)
+	if err != nil {
+		return err
+	}
+
+	var response = struct {
+		ChatID int    `json:"chat_id"`
+		Text   string `json:"text"`
+	}{
+		ChatID: t.Message.Chat.ID,
+		Text:   value,
+	}
+	b, err := json.Marshal(response)
+	if err != nil {
+		return err
+	}
+	res, err := http.Post(callbackURL, "application/json", bytes.NewReader(b))
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+	if res.StatusCode >= 400 {
+		io.Copy(os.Stderr, res.Body)
+		return fmt.Errorf("bad status: %d", res.StatusCode)
+	}
+	return nil
+}
+
+func lookup(msg string) (string, error) {
+	return fmt.Sprintf("value(%s)=%q", msg, envmap()[strings.ToUpper(msg)]), nil
+}
+
+func envmap() map[string]string {
+	m := map[string]string{}
+	for _, e := range os.Environ() {
+		kv := strings.Split(e, "=")
+		k := kv[0]
+		if !strings.HasPrefix(k, "BLIMPY_") {
+			continue
+		}
+		v := strings.Join(kv[1:], "=")
+		key := strings.ReplaceAll(k[len("BLIMPY_"):], "_", " ")
+		m[key] = v
+	}
+	return m
+}
